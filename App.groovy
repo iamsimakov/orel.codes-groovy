dc2b9d17334dfc7596888cd36d44210c7e2d32d7
Alexey Simakov <a.simakov@inventos.ru>
2018-11-12 23:54:02 +0300

    Fix update

---
+++ App.groovy
@@ -0,0 +1,30 @@
+/*
+ * Very simple HTTP server using http://sparkjava.com/
+ *
+ * Start the server with "groovy server.groovy".
+ */
+
+package codes.orel
+
+@Grab( group = 'com.sparkjava', module = 'spark-core', version = '2.1' )
+import static spark.Spark.*
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+import java.security.MessageDigest
+import java.text.SimpleDateFormat
+
+import codes.orel.AppResponse
+
+public class App {
+
+  static void main(String[] args) {
+    post '/', { req, res -> new AppResponse(req).resp }
+    
+    println 'Enter to exit'
+    System.in.newReader().readLine()
+    println "Done"
+    
+    stop()
+  }
+
+}
---
+++ AppResponse.groovy
@@ -0,0 +1,33 @@
+package codes.orel
+
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+import java.security.MessageDigest
+import java.text.SimpleDateFormat
+
+class AppResponse {
+
+  String resp
+
+  AppResponse(req) {
+    def jsonSlurper = new JsonSlurper()
+    def object = jsonSlurper.parseText(req.body())
+    object.first_name = "$object.first_name " + generateMD5(object.first_name)
+    object.last_name = "$object.last_name " + generateMD5(object.last_name)
+    object.current_time = date()
+    object.say = 'Just try Groovy!'
+    resp = JsonOutput.toJson(object)
+  }
+
+  def date() {
+    def curr = new Date()
+    def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+    sdf.format(curr)
+  }
+      
+  def generateMD5(String s){
+    if (s == null) s = '';
+    MessageDigest.getInstance("MD5").digest(s.bytes).encodeHex().toString()
+  }
+
+}
